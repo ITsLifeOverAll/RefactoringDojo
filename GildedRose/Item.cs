b40f9d0e95abbe72c3e0f47f5dedda7bbf861d18
Art Tsai <arttsai@gmail.com>
2024-03-01 09:02:35 +0800

    add project - Gilded Rose Kata

---
+++ GildedRose/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace GildedRose;
+
+public class Item
+{
+    public string Name { get; set; }
+    public int SellIn { get; set; }
+    public int Quality { get; set; }
+
+    public override string ToString()
+    {
+        return Name + ", " + SellIn + ", " + Quality;
+    }
+}
